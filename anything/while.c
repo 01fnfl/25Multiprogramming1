5cea700513ac36eb1903e7c11fb88ac8e460564a
01fnfl <01fnfl@gmail.com>
2025-04-14 17:09:53 +0900

    `"

---
+++ anything/while.c
@@ -0,0 +1,35 @@
+
+
+int main()
+{	
+	int o;
+	
+	o=3;
+
+	while(o)
+
+
+	{
+        	int i;
+	
+        	 i=3;
+
+         	while(i)
+         	{
+                 puts("tomato");
+                 i=i-1;
+
+        	 }	
+
+         	puts("sweet");
+
+
+		}	
+	
+	
+		o=o-1;
+
+	return 0;
+
+}
+
